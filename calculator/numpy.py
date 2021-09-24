e91543f5b968b01f828450a0e14475835f98892f
js <bada@ganada.kr>
2021-09-24 20:30:26 +0900

    20210924 init calculator

---
+++ calculator/numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pytest
+
+print(np.array([1, 2, 3, 4]))
+
+def capital_case(x):
+    return x.capitalize()
+
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
