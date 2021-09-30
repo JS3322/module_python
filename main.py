df084292ac98f8c668abafe0016eab4128918f9a
js <bada@ganada.kr>
2021-09-30 09:04:01 +0900

    20210930 fix module

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #from search_selenium import search
-import calculator.square2
+from calculator.square2 import base, square
+from calculator.person import Person
 # search()
 
-print(square2.base)
-print(square2.square(10))
+print(base)
+print(square(10))
+
+maria = Person('마리아', 20, '서울시 서초구 반포동')
+maria.greeting()
